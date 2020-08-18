a288ccdfaa4dcbccb706a5f34dc33e1123fa4594
kmahoney40 <kmahoney40@hotmail.com>
2020-08-18 15:41:41 -0700

    added curses

--- thread.py
+++ thread.py
@@ -1,85 +1,87 @@
 import threading
 import time
+import curses
 
+# These will become JSON
 sprinkler_dict = [1,2,3]
 daqc_dict = [0] 
 
+# This calss will read/write all the commands to run the sprinklers. At this point a single
+# relay-plate. The shared innput dictioary can be expanded to include weahter data that may be used
+# to dynamically modife runtimes.
 class sprinklerThread(threading.Thread):
     def __init__(self, threadID, name, counter, in_dict):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.counter = counter
         self.in_dict = in_dict
 
     def run(self):
-        print("Starting " + self.name)
+        #print("Starting " + self.name)
         while 1:
-            #threadLock.acquire()
-            print_time(self.name, self.counter, 1)
-            #threadLock.release()
             self.in_dict[0] += 1
             time.sleep(1)
 
-def print_time(threadName, delay, counter):
-    while counter:
-        time.sleep(delay)
-        print ("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
-
+# The daqcThread calss will read/write the daqc-plate and a relay-plate to monitor temps in and
+# outside of the garage and control an exhaust fan.
 class daqcThread(threading.Thread):
     def __init__(self, threadID, name, counter, in_dict):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.counter = counter
         self.in_dict = in_dict
 
     def run(self):
-        print("Starting " + self.name)
+        #print("Starting " + self.name)
         while 1:            
-            #threadLock.acquire()
-            print_time(self.name, self.counter, 1)
-            #threadLock.release()
             self.in_dict[0] += 1
             time.sleep(5)
 
-class uiThread(threading.Thread):
-    def __init__(self, threadID, name, counter, in_dict, daqc_dict):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-        self.in_dict = in_dict
-        self.daqc_dict = daqc_dict
 
-    def run(self):
-        print("Starting " + self.name)
-        while 1:
-            print_time(self.name, self.counter, 1)
-            print("in_dict " + str(self.in_dict[0]))
-            print("daqc_dict " + str(self.daqc_dict[0]))
-            time.sleep(1)
+def main(scr):
 
-threadLock = threading.Lock()
-threads = []
+    scr = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
+    scr.keypad(1)
+    scr.nodelay(1)
 
-# Create new threads
-thread1 = sprinklerThread(1, "sprinklerThread", 0, sprinkler_dict)
-thread2 = daqcThread(2, "daqcThread", 0, daqc_dict)
-thread3 = uiThread(3, "uiThread", 0, sprinkler_dict, daqc_dict)
+    threads = []
 
-# Start new Threads
-thread1.start()
-thread2.start()
-thread3.start()
+    # Create new threads
+    thread1 = sprinklerThread(1, "sprinklerThread", 0, sprinkler_dict)
+    thread2 = daqcThread(2, "daqcThread", 0, daqc_dict)
+    #thread3 = uiThread(3, "uiThread", 0, sprinkler_dict, daqc_dict, scr)
 
-# Add threads to thread list
-threads.append(thread1)
-threads.append(thread2)
+    # Start new Threads
+    thread1.start()
+    thread2.start()
 
-# Wait for all threads to complete
-for t in threads:
-    t.join()
-print ("Exiting Main Thread")
+    # Add threads to thread list
+    threads.append(thread1)
+    threads.append(thread2)
+    
+    print("After threads.append")
 
+    ct = 0
+    while ct < 10:
+        scr.addstr(5,5,"Woot: " + str(ct))
+        time.sleep(1)
+        scr.addstr(6,5, "sprinkler: " + str(sprinkler_dict[0]))
+        scr.addstr(7,5, "daqc: " + str(daqc_dict[0]))
+        scr.refresh()
+        ct += 1
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print ("Exiting Main Thread")
+
+    scr.refresh()
+
+    curses.endwin()
+
+if __name__ == '__main__':
+       
+    curses.wrapper(main)
+        
