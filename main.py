99b0f0caed703dce1f1ab74dab65909df57a82fd
kmahoney40 <kmahoney40@hotmail.com>
2020-08-19 13:48:15 -0700

    created files for the threads

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
 import threading
 import time
 import curses
+import sprinklerThread
+import tempThread
 
 # These will become JSON
 sprinkler_dict = [1,2,3]
 daqc_dict = [0] 
-
-# This calss will read/write all the commands to run the sprinklers. At this point a single
-# relay-plate. The shared innput dictioary can be expanded to include weahter data that may be used
-# to dynamically modife runtimes.
-class sprinklerThread(threading.Thread):
-    def __init__(self, threadID, name, in_dict, event):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.in_dict = in_dict
-        self.event = event
-
-    def run(self):
-        #print("Starting " + self.name)
-        while not self.event.is_set():
-            self.in_dict[0] += 1
-            #time.sleep(1)
-            self.event.wait(timeout=1)
-
-# The daqcThread calss will read/write the daqc-plate and a relay-plate to monitor temps in and
-# outside of the garage and control an exhaust fan.
-class daqcThread(threading.Thread):
-    def __init__(self, threadID, name, in_dict, event):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.in_dict = in_dict
-        self.event = event
-
-    def run(self):
-        #print("Starting " + self.name)
-        while not self.event.is_set():            
-            self.in_dict[0] += 1
-            #time.sleep(5)
-            self.event.wait(timeout=5)
-
 
 def main(scr):
 
@@ -54,8 +20,8 @@ def main(scr):
     threads = []
 
     # Create new threads
-    thread1 = sprinklerThread(1, "sprinklerThread", sprinkler_dict, e)
-    thread2 = daqcThread(2, "daqcThread", daqc_dict, e)
+    thread1 = sprinklerThread.sprinklerThread(1, "sprinklerThread", sprinkler_dict, e)
+    thread2 = tempThread.daqcThread(2, "daqcThread", daqc_dict, e)
 
     # Start new Threads
     thread1.start()
